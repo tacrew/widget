4b7f8d4bba52f597dbfccb29874e6ab072edc898
alisa <alisa@side.one>
2023-05-03 06:28:24 +0800

    feat: add example: npm install ping-widget

---
+++ example/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import pingWidget from 'ping-widget'
+</script>
+
+<template>
+  <div>
+    <div>Ping Widget Version: {{ pingWidget?.version }}</div>
+    <hello-ping />
+  </div>
+</template>
