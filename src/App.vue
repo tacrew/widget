e4fb6b77dddd8af53b105130714977e1e89789ce
alisa <alisa@side.one>
2023-05-03 06:44:04 +0800

    feat: test runtime

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // @ts-ignore
-import pingWidget from '../dist/ping-widget.js';
-// import pingWidget from '../lib/main';
+// import pingWidget from '../dist/ping-widget.js';
+import pingWidget from '../lib/main';
 </script>
 
 <template>
