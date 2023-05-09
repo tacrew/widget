e91a2626cd3bf4534d40476f0be29f777c2b7c16
Alisa | Side.one <alisa@side.one>
2023-05-09 22:21:38 +0800

    feat: add ping-widget@0.0.4 example

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,10 +1,71 @@
-<script setup>
-import pingWidget from 'ping-widget'
+<script setup lang="ts">
+import { ref } from 'vue';
+import pingWidget from 'ping-widget';
+
+const sender = 'juno1m8mma95ta2zajqtmfp5c5y3wgeyqzcrcgcnv4a';
+const endpoint = 'https://juno-api.polkachu.com';
+const chainId = 'juno-1';
+const hdPath = "m/44'/118/0'/0/0";
+
+const params = JSON.stringify({
+    proposal_id: '1',
+    validator_address: 'junovaloper1jxv0u20scum4trha72c7ltfgfqef6nscm9pmg2',
+});
+
+const types = [
+    'send',
+    'delegate',
+    'vote',
+    'redelegate',
+    'unbond',
+    'transfer',
+    'deposit',
+    'withdraw',
+    'withdraw_commission',
+];
+const toOpen = ref('send');
 </script>
 
 <template>
-  <div>
-    <div>Ping Widget Version: {{ pingWidget?.version }}</div>
-    <hello-ping />
-  </div>
+    <div>
+        Ping Widget Version: 0.0.4
+
+        <hello-ping :msg="pingWidget?.version" />
+
+        <ping-connect-wallet :chain-id="chainId" :hd-path="hdPath" />
+
+        <select v-model="toOpen">
+            <option v-for="i in types">{{ i }}</option>
+        </select>
+
+        <br />
+
+        <label :for="toOpen" class="btn">{{ toOpen }}</label>
+        <ping-tx-dialog
+            :type="toOpen"
+            :sender="sender"
+            :endpoint="endpoint"
+            :params="params"
+        ></ping-tx-dialog>
+
+        <br />
+        // example:<br />
+        <label for="withdraw" class="btn">Withdraw</label>
+        <ping-tx-dialog
+            type="withdraw"
+            :sender="sender"
+            :endpoint="endpoint"
+            :params="params"
+        ></ping-tx-dialog>
+
+        <label for="vote" class="btn">Vote</label>
+        <ping-tx-dialog
+            type="vote"
+            :sender="sender"
+            :endpoint="endpoint"
+            :params="params"
+        ></ping-tx-dialog>
+    </div>
 </template>
+
+<style scoped></style>
