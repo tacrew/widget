8882fd36ba0a2b06a2030fbb9f459f1dad89a623
liangping <18786721@qq.com>
2023-05-23 16:18:53 +0800

    add Ledger wallet

--- lib/wallet/Wallet.ts
+++ lib/wallet/Wallet.ts
@@ -1,32 +1,28 @@
-import { fromBase64, fromBech32, toHex } from "@cosmjs/encoding";
-import { SignerData, StdFee,  } from '@cosmjs/stargate'
-import { EncodeObject,   
+import { fromBase64, fromBech32, toHex, toBase64 } from "@cosmjs/encoding";
+import { AminoTypes, createDefaultAminoConverters, } from '@cosmjs/stargate'
+import {
     encodePubkey,
-    GeneratedType,
-    isOfflineDirectSigner,
     makeAuthInfoBytes,
     makeSignDoc,
-    OfflineSigner,OfflineDirectSigner,
     Registry,
-    TxBodyEncodeObject,} from '@cosmjs/proto-signing'
-import { sha256, stringToPath } from '@cosmjs/crypto'
+    TxBodyEncodeObject,
+} from '@cosmjs/proto-signing'
+import { stringToPath } from '@cosmjs/crypto'
 import { encodeSecp256k1Pubkey, makeSignDoc as makeSignDocAmino } from "@cosmjs/amino";
-import {
-    MsgDelegateEncodeObject,
-    MsgSendEncodeObject,
-    MsgTransferEncodeObject,
-    MsgUndelegateEncodeObject,
-    MsgWithdrawDelegatorRewardEncodeObject,
-    defaultRegistryTypes
-  } from "@cosmjs/stargate";
+import { defaultRegistryTypes } from "@cosmjs/stargate";
 // ledger
 import TransportWebBLE from '@ledgerhq/hw-transport-web-ble'
 import TransportWebUSB from '@ledgerhq/hw-transport-webusb'
-import CosmosApp from 'ledger-cosmos-js'
 import { LedgerSigner } from '@cosmjs/ledger-amino'
-import { ethToEvmos } from '@tharsis/address-converter'
 import { SignMode } from "cosmjs-types/cosmos/tx/signing/v1beta1/signing";
 import { TxRaw, SignDoc } from "cosmjs-types/cosmos/tx/v1beta1/tx";
+import { Transaction } from "../utils/type";
+import { Any } from "cosmjs-types/google/protobuf/any";
+import { PubKey } from 'cosmjs-types/cosmos/crypto/secp256k1/keys'
+import { Chain, createTxRawEIP712, signatureToWeb3Extension } from "@tharsis/transactions";
+import { createEIP712, generateFee, generateMessageWithMultipleTransactions, generateTypes } from "@tharsis/eip712";
+import { defaultMessageAdapter } from "./EthermintMessageAdapter";
+import { createTransactionWithMultipleMessages } from "@tharsis/proto/dist/transaction/transaction";
 
 export enum WalletName {
     Keplr = "Keplr",
@@ -41,7 +37,7 @@ export enum WalletName {
 export interface ConnectedWallet {
     wallet: WalletName,
     cosmosAddress: string
-    hdPath?: string 
+    hdPath?: string
 }
 
 export interface Account {
@@ -59,15 +55,23 @@ export interface WalletArgument {
 }
 
 export interface AbstractWallet {
-  name: WalletName  
-  /**
-   * The the accounts from the wallet (addresses)
-   */
-  getAccounts(): Promise<Account[]>
-  supportCoinType(coinType?: string): Promise<boolean>
-  sign(signerAddress: string, signDoc: SignDoc): Promise<any>
+    name: WalletName
+    /**
+     * The the accounts from the wallet (addresses)
+     */
+    getAccounts(): Promise<Account[]>
+    supportCoinType(coinType?: string): Promise<boolean>
+    sign(transaction: Transaction): Promise<any>
 }
 
+function extractChainId(chainId: string) {
+    const start = chainId.indexOf('_')
+    const end = chainId.indexOf('-')
+    if (end > start && start > 0) {
+      return Number(chainId.substring(start + 1, end))
+    }
+    return 0
+}
 
 const DEFAULT_HDPATH = "m/44'/118/0'/0/0";
 
@@ -76,86 +80,43 @@ export function readWallet(hdPath?: string) {
         localStorage.getItem(hdPath || DEFAULT_HDPATH) || '{}'
     ) as ConnectedWallet
 }
-export function writeWallet(connected: ConnectedWallet , hdPath?: string) {
-    localStorage.setItem( hdPath || DEFAULT_HDPATH,JSON.stringify(connected))
+export function writeWallet(connected: ConnectedWallet, hdPath?: string) {
+    localStorage.setItem(hdPath || DEFAULT_HDPATH, JSON.stringify(connected))
 }
 
 export function removeWallet(hdPath?: string) {
     localStorage.removeItem(hdPath || DEFAULT_HDPATH);
 }
 
-export function createWallet(name: WalletName, arg: WalletArgument) : AbstractWallet {
-    console.log("wallet arg:", arg)
+export function createWallet(name: WalletName, arg: WalletArgument, registry?: Registry): AbstractWallet {
+    const reg = registry || new Registry(defaultRegistryTypes)
     switch (name) {
         case WalletName.Keplr:
-            return new KeplerWallet(arg)
-        // case WalletName.Ledger:
-        //     return new LedgerWallet(arg)
-        // case WalletName.LedgerBLE:
-        //     arg.transport = "BLE"
-        //     return new LedgerWallet(arg)
-        // case WalletName.Metamask:
-        //     return new KeplerWallet(arg)
-        // case WalletName.Address:
-        //     return new AddressWallet(arg)
-        // case WalletName.NameService:
-        //     return new NameServiceWallet(arg)
+            return new KeplerWallet(arg, reg)
+        case WalletName.Ledger:
+            return new LedgerWallet(arg, reg)
     }
     throw new Error("No wallet created")
 }
-
-// export class AddressWallet implements AbstractWallet {
-//     name: WalletName.Address
-//     address: string
-
-//     constructor(arg: WalletArgument) {
-//         this.address = arg.address|| ""
-//     }
-
-//     getAccounts(): Promise<Account[]> {
-//         throw new Error('Method not implemented.')
-//     }
-//     supportCoinType(coinType?: string | undefined): Promise<boolean> {
-//         return Promise.resolve(false);
-//     }
-//     sign(transaction: { chainId: string; signerAddress: string; messages: readonly EncodeObject[]; fee: StdFee; memo: string; signerData: SignerData }): Promise<any> {
-//         throw new Error('Method not implemented.')
-//     }    
-// }
-
-// export class NameServiceWallet implements AbstractWallet {
-//     name: WalletName.NameService
-//     address: string[]
-//     constructor(arg: WalletArgument) {
-//         this.address = [arg.address|| ""]
-//     }
-//     getAccounts(): Promise<any> {
-//         throw new Error('Method not implemented.')
-//     }
-//     supportCoinType(coinType?: string | undefined): Promise<boolean> {
-//         return Promise.resolve(false);
-//     }
-//     sign(transaction: { chainId: string; signerAddress: string; messages: readonly EncodeObject[]; fee: StdFee; memo: string; signerData: SignerData }): Promise<any> {
-//         throw new Error('Method not implemented.')
-//     }    
-// }
 
 export class KeplerWallet implements AbstractWallet {
     name: WalletName.Keplr
     chainId: string
     registry: Registry
-    constructor(arg: WalletArgument) {
+    conf: WalletArgument
+    constructor(arg: WalletArgument, registry: Registry) {
         this.chainId = arg.chainId || "cosmoshub"
         // @ts-ignore
         if (!window.getOfflineSigner || !window.keplr) {
             throw new Error('Please install keplr extension')
         }
-        this.registry = new Registry(defaultRegistryTypes)
+        this.registry = registry
+        this.conf = arg
     }
     async getAccounts(): Promise<Account[]> {
         // const chainId = 'cosmoshub'
         // @ts-ignore
-        await window.keplr.enable(this.chainId )
+        await window.keplr.enable(this.chainId)
         // @ts-ignore
         const offlineSigner = window.getOfflineSigner(this.chainId)
         return offlineSigner.getAccounts()
@@ -163,56 +124,235 @@ export class KeplerWallet implements AbstractWallet {
     supportCoinType(coinType?: string | undefined): Promise<boolean> {
         return Promise.resolve(true);
     }
-    async sign( signerAddress: string, signDoc: SignDoc ): Promise<any> {
+    isEthermint() {
+        return this.conf.hdPath && this.conf.hdPath.startsWith("m/44'/60")
+    }
+    async sign(transaction: Transaction): Promise<TxRaw> {
+        const accouts = await this.getAccounts()
+        const hex = toHex(fromBech32(transaction.signerAddress).data)
+        const accountFromSigner = accouts.find((account) => toHex(fromBech32(account.address).data) === hex);
+        if (!accountFromSigner) {
+            throw new Error("Failed to retrieve account from signer");
+        }
+        const pubkey = this.isEthermint() ? Any.fromPartial({
+            typeUrl: '/ethermint.crypto.v1.ethsecp256k1.PubKey',
+            value: PubKey.encode({
+                key: accountFromSigner.pubkey,
+            }).finish(),
+        }) : encodePubkey(encodeSecp256k1Pubkey(accountFromSigner.pubkey));
+        const txBodyEncodeObject: TxBodyEncodeObject = {
+            typeUrl: "/cosmos.tx.v1beta1.TxBody",
+            value: {
+                messages: transaction.messages,
+                memo: transaction.memo,
+            },
+        };
+        console.log(txBodyEncodeObject, transaction.messages)
+        const txBodyBytes = this.registry.encode(txBodyEncodeObject);
+        const gasLimit = Number(transaction.fee.gas);
+        const authInfoBytes = makeAuthInfoBytes(
+            [{ pubkey, sequence: transaction.signerData.sequence }],
+            transaction.fee.amount,
+            gasLimit,
+            transaction.fee.granter,
+            transaction.fee.payer,
+        );
+        const signDoc = makeSignDoc(txBodyBytes, authInfoBytes, transaction.chainId, transaction.signerData.accountNumber);
+
         // @ts-ignore
         const offlineSigner = window.getOfflineSigner(this.chainId)
-        return await offlineSigner.signDirect(signerAddress, signDoc);
-    }    
+        const { signature, signed } = await offlineSigner.signDirect(transaction.signerAddress, signDoc);;
+        return TxRaw.fromPartial({
+            bodyBytes: signed.bodyBytes,
+            authInfoBytes: signed.authInfoBytes,
+            signatures: [fromBase64(signature.signature)],
+        });
+    }
 }
 
+export class LedgerWallet implements AbstractWallet {
+    name: WalletName.Ledger
+    transport: string
+    hdPath: string
+    registry: Registry
+    aminoTypes = new AminoTypes(createDefaultAminoConverters())
+    conf: WalletArgument
+    constructor(arg: WalletArgument, registry: Registry) {
+        this.transport = arg.transport || 'usb'
+        this.hdPath = arg.hdPath || ''
+        this.registry = registry
+        this.conf = arg
+        console.log(arg, registry)
+    }
 
-// export class LedgerWallet implements AbstractWallet {
-//     name: WalletName.Ledger
-//     transport: string
-//     hdPath: string
-    
-//     constructor(arg: WalletArgument) {
-//         this.transport = arg.transport || 'usb'
-//         this.hdPath = arg.hdPath || ''
-//     }
-//     getSigner() {
-//         const transport = this.transport === 'usb' ? await TransportWebUSB.create() : await TransportWebBLE.create()
-//         // extract Cointype from from HDPath
-//         const coinType = Number(stringToPath(this.hdPath)[1])
-//         let ledgerAppName = 'Cosmos'
-//         switch (coinType) {
-//             // case 60:
-//             //  return EthereumLedgerSigner.create(device, hdpath) // 'Ethereum'
-//             case 529:
-//                 ledgerAppName = 'Secret' // 'Secret'
-//                 break
-//             case 852:
-//                 ledgerAppName = 'Desmos' // 'Desmos'
-//                 break
-//             case 330:
-//                 ledgerAppName = 'Terra' // 'Terra'
-//                 break
-//             case 118:
-//             default:
-//         }
-//         return new LedgerSigner(transport, { hdPaths: [stringToPath(this.hdPath)], ledgerAppName })
-//     }
-    
-//     async getAccounts(): Promise<Account[]> {
-//         const account = this.getSigner().getAccounts()
-//         return account
-//     }
-//     supportCoinType(coinType?: string | undefined): Promise<boolean> {
-//         return Promise.resolve(true);
-//     }
-//     sign(signerAddress: string, signDoc: SignDoc): Promise<any> {
-//         return this.getSigner().signAmino(signerAddress, signDoc)
-//     }
+    async getSigner() {
+        const transport = this.transport === 'usb' ? await TransportWebUSB.create() : await TransportWebBLE.create()
+        // extract Cointype from from HDPath
+        const hdPath = stringToPath(this.hdPath)
+        const coinType = Number(hdPath[1])
+        let ledgerAppName = 'Cosmos'
+        switch (coinType) {
+            case 60:
+               throw new Error("Not support.")
+            case 529:
+                ledgerAppName = 'Secret' // 'Secret'
+                break
+            case 852:
+                ledgerAppName = 'Desmos' // 'Desmos'
+                break
+            case 330:
+                ledgerAppName = 'Terra' // 'Terra'
+                break
+            case 118:
+            default:
+        }
+        // const path = stringToPath(this.hdPath)
+        return new LedgerSigner(transport, { ledgerAppName })
+    }
 
-// }
+    async getAccounts(): Promise<Account[]> {
+        const signer = await this.getSigner();
+        const account = (await signer.getAccounts()).map(x => ({
+            address: x.address,
+            algo: x.algo,
+            pubkey: x.pubkey,
+        }))
+        return account
+    }
+    supportCoinType(coinType?: string | undefined): Promise<boolean> {
+        return Promise.resolve(true);
+    }
+    isEthermint() {
+        return this.conf.hdPath && this.conf.hdPath.startsWith("m/44'/60")
+    }
 
+    async sign(tx: Transaction): Promise<TxRaw | Uint8Array> {
+        // if (this.isEthermint()) {
+        //     return this.sign712(tx)
+        // }
+        return this.signAmino(tx)
+    }
+    goEthAddress(address) {
+        return `0x${toHex(fromBech32(address).data)}`
+    }
+    // not finished, due to the outdated dependency.
+    async sign712(tx: Transaction): Promise<Uint8Array> {
+        const chain: Chain = {
+            chainId: extractChainId(tx.signerData.chainId),
+            cosmosChainId: tx.chainId,
+        }
+
+        const signer = await this.getSigner()
+        const ethAddr = this.goEthAddress(tx.signerAddress)
+
+        // this.signer.prefix = fromBech32(signerAddress).prefix
+        const account = await signer.getAccounts()
+
+        const acc = account.find(x => x.address === ethAddr)
+        if (!acc) {
+            throw new Error('The signer address dose not exsits in Ledger!')
+        }
+        const sender = {
+            accountAddress: tx.signerAddress,
+            sequence: tx.signerData.sequence,
+            accountNumber: tx.signerData.accountNumber,
+            pubkey: toBase64(account[0].pubkey),
+        }
+
+        const fees = generateFee(tx.fee.amount[0].amount, tx.fee.amount[0].denom, tx.fee.gas, tx.signerAddress)
+
+        const msgs = tx.messages.map(x => this.aminoTypes.toAmino(x))
+        const toSignTx = generateMessageWithMultipleTransactions(
+            sender.accountNumber.toString(),
+            sender.sequence.toString(),
+            tx.signerData.chainId,
+            tx.memo,
+            fees,
+            msgs,
+        )
+
+        const types = generateTypes(defaultMessageAdapter[tx.messages[0].typeUrl].getTypes())
+        const eip = createEIP712(types, chain.chainId, toSignTx)
+
+        // @ts-ignore
+        const sig = await this.getSigner().sign712(eip)
+
+        const rawTx = this.makeRawTxEvmos(sender, tx.messages, tx.memo, tx.fee, sig, chain)
+        return rawTx
+    }
+
+    makeRawTxEvmos(sender, messages, memo, fee, signature, chain): Uint8Array {
+        /// evmos style
+        /// *
+        const protoMsgs = messages.map(x => {
+          const adapter = defaultMessageAdapter[x.typeUrl]
+          return adapter.toProto(x)
+        })
+      
+        const evmos = createTransactionWithMultipleMessages(
+          protoMsgs,
+          memo,
+          fee.amount[0].amount,
+          fee.amount[0].denom,
+          Number(fee.gas),
+          'ethsecp256',
+          sender.pubkey,
+          sender.sequence,
+          sender.accountNumber,
+          chain.cosmosChainId,
+        )
+      
+        const extension = signatureToWeb3Extension(chain, sender, signature)
+      
+        // Create the txRaw
+        const prototx = createTxRawEIP712(evmos.legacyAmino.body, evmos.legacyAmino.authInfo, extension)
+        return prototx.message.serializeBinary()
+        /// end of EVMOS style */
+      }
+      
+
+    async signAmino(tx: Transaction): Promise<TxRaw> {
+        const signer = await this.getSigner();
+        // assert(!isOfflineDirectSigner(signer));
+        const signerAddress = tx.signerAddress
+
+        const accountFromSigner = (await signer.getAccounts()).find(
+            (account) => account.address === signerAddress,
+        );
+        if (!accountFromSigner) {
+            throw new Error("Failed to retrieve account from signer");
+        }
+        const pubkey = encodePubkey(encodeSecp256k1Pubkey(accountFromSigner.pubkey));
+        const signMode = SignMode.SIGN_MODE_LEGACY_AMINO_JSON;
+        const msgs = tx.messages.map((msg) => this.aminoTypes.toAmino(msg));
+        const signDoc = makeSignDocAmino(msgs, tx.fee, tx.chainId, tx.memo, tx.signerData.accountNumber, tx.signerData.sequence);
+        const { signature, signed } = await signer.signAmino(signerAddress, signDoc);
+
+        const signedTxBody = {
+            messages: signed.msgs.map((msg) => this.aminoTypes.fromAmino(msg)),
+            memo: signed.memo,
+        };
+        const signedTxBodyEncodeObject: TxBodyEncodeObject = {
+            typeUrl: "/cosmos.tx.v1beta1.TxBody",
+            value: signedTxBody,
+        };
+        const signedTxBodyBytes = this.registry.encode(signedTxBodyEncodeObject);
+
+        const signedGasLimit = Number(signed.fee.gas);
+        const signedSequence = Number(signed.sequence);
+        const signedAuthInfoBytes = makeAuthInfoBytes(
+            [{ pubkey, sequence: signedSequence }],
+            signed.fee.amount,
+            signedGasLimit,
+            signed.fee.granter,
+            signed.fee.payer,
+            signMode,
+        );
+        return TxRaw.fromPartial({
+            bodyBytes: signedTxBodyBytes,
+            authInfoBytes: signedAuthInfoBytes,
+            signatures: [fromBase64(signature.signature)],
+        });
+    }
+
+}
