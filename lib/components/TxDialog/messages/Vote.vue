1cf5ffee5217f022f6c828b575bdc8a636f2840f
liangping <18786721@qq.com>
2023-05-27 13:43:35 +0800

    fix: set default vote to yes

--- lib/components/TxDialog/messages/Vote.vue
+++ lib/components/TxDialog/messages/Vote.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 });
 
 const params = JSON.parse(props.params || "{}")
-const option = ref("")
+const option = ref("1")
 
 const msgs = computed(() => {
     return [{
@@ -53,7 +53,7 @@ defineExpose({msgs, isValid, initial})
                 <span class="label-text">Option</span>
             </label>
             <div class="flex">
-                <input v-model="option" type="radio" id="yes" value="1" class="radio radio-success mx-2" checked /><label for="yes"> Yes</label> 
+                <input v-model="option" type="radio" id="yes" value="1" class="radio radio-success mx-2"/><label for="yes"> Yes</label> 
                 <input v-model="option" type="radio" id="no" value="3" class="radio radio-error mx-2" /><label for="no"> No</label> 
                 <input v-model="option" type="radio" id="veto" value="4" class="radio radio-error mx-2" /><label for="veto"> No With Veto</label> 
                 <input v-model="option" type="radio" id="abstain" value="2" class="radio radio-dark mx-2" /><label for="abstain"> Abstain</label> 
