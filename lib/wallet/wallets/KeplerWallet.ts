7dd11398220f40a283c13c6d4f5dc8cce6117e82
liangping <18786721@qq.com>
2023-05-30 09:36:35 +0800

    imp: move implementation to its own file

---
+++ lib/wallet/wallets/KeplerWallet.ts
@@ -0,0 +1,79 @@
+import { fromBase64, fromBech32, toHex } from "@cosmjs/encoding";
+import { Registry, TxBodyEncodeObject, encodePubkey, makeAuthInfoBytes, makeSignDoc } from "@cosmjs/proto-signing"
+import { AbstractWallet, Account, WalletArgument, WalletName } from "../Wallet"
+import { Transaction } from "../../utils/type"
+import { TxRaw } from "cosmjs-types/cosmos/tx/v1beta1/tx";
+import { Any } from "cosmjs-types/google/protobuf/any";
+import { PubKey } from 'cosmjs-types/cosmos/crypto/secp256k1/keys'
+import { encodeSecp256k1Pubkey } from "@cosmjs/amino";
+
+export class KeplerWallet implements AbstractWallet {
+    name: WalletName.Keplr
+    chainId: string
+    registry: Registry
+    conf: WalletArgument
+    constructor(arg: WalletArgument, registry: Registry) {
+        this.chainId = arg.chainId || "cosmoshub"
+        // @ts-ignore
+        if (!window.getOfflineSigner || !window.keplr) {
+            throw new Error('Please install keplr extension')
+        }
+        this.registry = registry
+        this.conf = arg
+    }
+    async getAccounts(): Promise<Account[]> {
+        // const chainId = 'cosmoshub'
+        // @ts-ignore
+        await window.keplr.enable(this.chainId)
+        // @ts-ignore
+        const offlineSigner = window.getOfflineSigner(this.chainId)
+        return offlineSigner.getAccounts()
+    }
+    supportCoinType(coinType?: string | undefined): Promise<boolean> {
+        return Promise.resolve(true);
+    }
+    isEthermint() {
+        return this.conf.hdPath && this.conf.hdPath.startsWith("m/44'/60")
+    }
+    async sign(transaction: Transaction): Promise<TxRaw> {
+        const accouts = await this.getAccounts()
+        const hex = toHex(fromBech32(transaction.signerAddress).data)
+        const accountFromSigner = accouts.find((account) => toHex(fromBech32(account.address).data) === hex);
+        if (!accountFromSigner) {
+            throw new Error("Failed to retrieve account from signer");
+        }
+        const pubkey = this.isEthermint() ? Any.fromPartial({
+            typeUrl: '/ethermint.crypto.v1.ethsecp256k1.PubKey',
+            value: PubKey.encode({
+                key: accountFromSigner.pubkey,
+            }).finish(),
+        }) : encodePubkey(encodeSecp256k1Pubkey(accountFromSigner.pubkey));
+        const txBodyEncodeObject: TxBodyEncodeObject = {
+            typeUrl: "/cosmos.tx.v1beta1.TxBody",
+            value: {
+                messages: transaction.messages,
+                memo: transaction.memo,
+            },
+        };
+        console.log(txBodyEncodeObject, transaction.messages)
+        const txBodyBytes = this.registry.encode(txBodyEncodeObject);
+        const gasLimit = Number(transaction.fee.gas);
+        const authInfoBytes = makeAuthInfoBytes(
+            [{ pubkey, sequence: transaction.signerData.sequence }],
+            transaction.fee.amount,
+            gasLimit,
+            transaction.fee.granter,
+            transaction.fee.payer,
+        );
+        const signDoc = makeSignDoc(txBodyBytes, authInfoBytes, transaction.chainId, transaction.signerData.accountNumber);
+
+        // @ts-ignore
+        const offlineSigner = window.getOfflineSigner(this.chainId)
+        const { signature, signed } = await offlineSigner.signDirect(transaction.signerAddress, signDoc);;
+        return TxRaw.fromPartial({
+            bodyBytes: signed.bodyBytes,
+            authInfoBytes: signed.authInfoBytes,
+            signatures: [fromBase64(signature.signature)],
+        });
+    }
+}
