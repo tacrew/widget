cf558bb06ffdcae0feb9a7e3040636e10163e81e
liangping <18786721@qq.com>
2023-12-13 19:15:34 +0800

    remove unused import

--- lib/components/TxDialog/messages/WithdrawCommission.vue
+++ lib/components/TxDialog/messages/WithdrawCommission.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed, onMounted, ref } from 'vue';
+import { computed, ref } from 'vue';
 import { getDelegateRewards } from '../../../utils/http';
 
 const props = defineProps({
